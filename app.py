04972eb4d3b6e90f5e576fad664fca8b2a1598cd
Kyle Moore <116331941+kmoore1616@users.noreply.github.com>
2024-12-10 14:16:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, login_required, UserMixin, logout_user, current_user
+from sqlalchemy.exc import IntegrityError
 
+app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = **TODO Need to implement database**
+app.config['SECRET_KEY'] = "act07-app1.py.pngDownload act07-app1.py.png (282 KB)"
+db = SQLAlchemy(app)
