45170df7199fb9250b1780667152e91bb813557a
zooeyuhey <zoe.wolfe@western.edu>
2024-12-10 14:32:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,15 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'stocks.sqlite'
 app.config['SECRET_KEY'] = "act07-app1.py.pngDownload act07-app1.py.png (282 KB)"
 db = SQLAlchemy(app)
+
+@app.route('/create', methods =['POST','GET'])
+def create():
+    if request.method == 'GET':
+        return render_template('create.html')
+    elif request.method == 'POST':
+        username = request.form['swimmer']
+        password = request.form['height']
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return render_template('portfolio.html')
