bc81d3887956406dd7941e30e08c39c48481109b
Kyle Moore <116331941+kmoore1616@users.noreply.github.com>
2024-12-11 16:34:03 -0700

    Update code.js

--- static/code.js
+++ static/code.js
@@ -1,1 +1,24 @@
+var socket = io();
+var key = 46c5a0a4eba75f027bf2623f2e948972;
 
+
+function get_stock_info(symbol, date){
+                var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+                                if(this.readyState === 4 && this.status === 200){
+                                                var json = parse(this.responseText);
+                                                console.log(json);
+                                                return json;
+                                }
+                                xhttp.open("GET", "http://api.marketstack.com/v1/eod/" + date + "? access_key = 46c5a0a4eba75f027bf2623f2e948972& symbols = "+ symbol);
+                                xhttp.send();
+                }
+};
+
+
+socket.on("get_stock_info", function(symbol, date){
+                var stock_info = [];
+                stock_info.push(get_stock_info(symbol, date));
+                stock_info.push(get_stock_info(symbol, "latest"));
+                socket.send(stock_info);
+});
