d934b61871d2881cec4a541f319490bbcca4c626
Kyle Moore <116331941+kmoore1616@users.noreply.github.com>
2024-12-12 13:58:25 -0700

    Update code.js

--- static/code.js
+++ static/code.js
@@ -2,23 +2,20 @@ var socket = io();
 var key = 46c5a0a4eba75f027bf2623f2e948972;
 
 
-function get_stock_info(symbol, date){
-                var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-                                if(this.readyState === 4 && this.status === 200){
-                                                var json = parse(this.responseText);
-                                                console.log(json);
-                                                return json;
-                                }
-                                xhttp.open("GET", "http://api.marketstack.com/v1/eod/" + date + "? access_key = 46c5a0a4eba75f027bf2623f2e948972& symbols = "+ symbol);
-                                xhttp.send();
+function deposit(){
+                var amount = document.getElementById("cash_add").value;
+                if(amount <= 0 || !(typeof amount === "number")){
+                                alert("Invalid entry. Enter a positive number...");
+                }else{
+                                socket.emit("deposit", amount);
                 }
-};
+}
 
-
-socket.on("get_stock_info", function(symbol, date){
-                var stock_info = [];
-                stock_info.push(get_stock_info(symbol, date));
-                stock_info.push(get_stock_info(symbol, "latest"));
-                socket.send(stock_info);
-});
+function withdraw(){
+                var amount = document.getElementById("cash_remove").value;
+                if(amount <= 0 || !(typeof amount === "number")){
+                                alert("Invalid entry. Enter a positive number...");
+                }else{
+                                socket.emit("withdraw", amount);
+                }
+}
